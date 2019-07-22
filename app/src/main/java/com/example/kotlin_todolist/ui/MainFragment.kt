67bb88aba8174fba7ec567764b769745e03c200d
parkjunghoon <go9018@naver.com>
2019-07-22 20:52:02 +0900

    add : navigation 을 사용한 화면전환

--- app/src/main/java/com/example/kotlin_todolist/ui/MainFragment.kt
+++ app/src/main/java/com/example/kotlin_todolist/ui/MainFragment.kt
@@ -6,17 +6,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.kotlin_todolist.R
+import kotlinx.android.synthetic.main.fragment_main.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- *
- */
 class MainFragment : Fragment() {
 
     override fun onCreateView(
@@ -25,6 +18,13 @@ class MainFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        add_button.setOnClickListener {
+            it.findNavController().navigate(R.id.action_mainFragment_to_editFragment)
+        }
     }
 
 
